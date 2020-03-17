a2c0fd5500c5930234f03285b9d1fab81da6eaf0
Victor Antunez <victorac94@hotmail.com>
2020-03-17 17:24:34 +0100

    Añadidos componentes de la ruta /home al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginRegisterComponent } from './login-register/login-register.component';
-
+import { AddpostComponent } from './addpost/addpost.component';
+import { HeaderComponent } from './header/header.component';
+import { MycategoriesComponent } from './mycategories/mycategories.component';
+import { PostComponent } from './post/post.component';
+import { TrenddingComponent } from './trendding/trendding.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'register', component: LoginRegisterComponent },
   { path: 'login', component: LoginRegisterComponent },
-  { path: 'register', component: LoginRegisterComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'addpost', component: AddpostComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'mycategories', component: MycategoriesComponent },
+  { path: 'trendding', component: TrenddingComponent },
+  { path: 'user', component: UserComponent },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/app.module 2.ts
+++ src/app/app.module 2.ts
@@ -4,27 +4,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TrenddingComponent } from './trendding/trendding.component';
-import { MycategoriesComponent } from './mycategories/mycategories.component';
-import { AddpostComponent } from './addpost/addpost.component';
-import { PostComponent } from './post/post.component';
-import { UserComponent } from './user/user.component';
 import { LoginRegisterComponent } from './login-register/login-register.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TrenddingComponent,
-    MycategoriesComponent,
-    AddpostComponent,
-    PostComponent,
-    UserComponent,
-    LoginRegisterComponent,
-    HomeComponent
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
